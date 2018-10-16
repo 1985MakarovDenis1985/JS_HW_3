fe183510b28a7f2acfe840de55c524d379889513
Denis, Makarov <makarovdenis07081985@gmail.com>
2018-10-16 18:57:32 +0300

    completely

--- HW_2/js/description.js
+++ HW_2/js/description.js
@@ -1,35 +1,54 @@
 'use strict';
 
+var numberOne;
+var numberTwo;
+
+mainQestion()
 
 
-function askFirstNumber() {
-  return +prompt('Enter please first number...');
-}
+function mainQestion() {
 
-function askSecondNumber() {
-  return +prompt('Enter please second number...')
-}
+  ask_numberOne();
+
+  if(numberOne !== 0){
+    ask_numberTwo();
+
+  if(numberOne === 0 || numberTwo === 0){
+    alert('see you next time')
+    return;
+  }else if(numberTwo < numberOne){
+    alert('First number had to lower than second! Try again...');
+    return mainQestion();
+  }else if(isNaN(numberOne) || isNaN(numberTwo)){
+    alert('anything was not number')
+    return mainQestion()
+  }else{
+    calcSum();
+    alert(calcSum());
+  }
+}else if (numberOne === 0){
+  alert('See you later')
+  return;
+  };
+
+
+function ask_numberOne() {
+  numberOne = +prompt("first number please");
+  return numberOne;
+  };
+
+function ask_numberTwo() {
+  numberTwo = +prompt("second number please");
+  return numberTwo;
+  };
 
 function calcSum(){
   var sum = 0;
-  var i = askFirstNumber();
-  var secondNumber = askSecondNumber();
-  var x = 'Good Work!'
-
-  if(i > secondNumber){
-    alert('First number had to lower than second! Try again...');
-    return calcSum();
-  }else if(isNaN(i) || isNaN(secondNumber)){
-    alert('Sorry, anithing wasn`t number')
-    return calcSum();
-  }else if(i == 0 || secondNumber == 0){
-    return('Ok! See you later..')
-  }
-
+  var i = numberOne;
+  var secondNumber = numberTwo;
   for(i ; i <= secondNumber; i++){
-    sum = sum + i;
-  }
+      sum = sum + i;
+    };
   return sum + "\nGood work!!!";
-};
-
-alert(calcSum());
+  };
+}
